f6af86bd9b5bfa23fad7de324db54ddfef29b2ac
withbeast <15838601879@163.com>
2023-05-28 08:36:04 +0800

    benchdata

--- single_brunel.sh
+++ single_brunel.sh
@@ -1,11 +1,17 @@
 
-nsyns=(10000000 20000000 30000000 40000000 50000000 60000000 70000000 80000000 90000000 100000000)
-
+# nsyns=(10000000 20000000 30000000 40000000 50000000 60000000 70000000 80000000 90000000 100000000)
+nsyns=(20000000 40000000 60000000 80000000 100000000 120000000 140000000 160000000 180000000 200000000)
+rates=(0.01 0.05 0.1)
+cd build/bin
+chmod +x lif_unit_test
 #输出文字信息
 for nsyn in "${nsyns[@]}"
 do
-    nvprof /home/cuimy/zfz/bsim-test/build/bin/lif_unit_test --nsyn=$nsyn --net=brunel 2>&1 | tee /home/cuimy/zfz/bsim-test/benchdata/single/bsim_brunel_${nsyn}_nvprof.txt
-    echo done:$nsyn
+    for rate in "${rates[@]}"
+    do
+        nvprof ./lif_unit_test --nsyn=$nsyn --net=brunel --rate=$rate > ../../benchdata/single2/bsim_single_brunel_${nsyn}_${rate}_nvprof.txt 2>&1
+        echo done:$nsyn,$rate
+    done
 done
 #输出nvvp文件信息
 # for nsyn in "${nsyns[@]}"
